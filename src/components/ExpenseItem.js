0f49cf91f23f38a11c46f1f7c3aeaf773a791f5b
mikaemul <mikael.multanen@metropolia.fi>
2022-01-12 12:52:16 +0200

    12.1.2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return (
-        <h2>Expense Item</h2>
-    );
+  const expenseDate = new Date(2022, 1, 12);
+  const expenseTitle = "House Insurance";
+  const expenseAmount = 300.5;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
